373eb718667a9be8edec8e8d4610ab81140695a9
Santiago <santiago.pagnanelli12@gmail.com>
2022-11-07 11:51:57 -0300

    db finish

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    height_min: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    height_max: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight_min: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight_max: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    lifeExpectancy: {
+      type: DataTypes.INTEGER,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://pbs.twimg.com/media/D6mH_epWwAAl8Yn.jpg',
+    },
+  }, { timestamps: false }
+  );
 };
