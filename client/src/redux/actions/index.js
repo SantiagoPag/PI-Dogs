bcbae011bf0371efe66b2d744d5b39d61e5c0f29
Santiago <santiago.pagnanelli12@gmail.com>
2022-11-14 11:36:15 -0300

    finished actions/reducer

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -4,6 +4,11 @@ export const GET_DOG_NAME = 'GET_DOG_NAME';
 export const GET_DOG_DETAIL = 'GET_DOG_DETAIL';
 export const GET_ALL_TEMPERAMENTS = 'GET_ALL_TEMPERAMENTS';
 export const POST_DOG = 'POST_DOG';
+export const FILTER_BY_NAME = 'FILTER_BY_NAME';
+export const FILTER_BY_TEMPERAMENTS = 'FILTER_BY_TEMPERAMENTS';
+export const FILTER_BY_WEIGHT = 'FILTER_BY_WEIGHT';
+export const FILTER_CREATED_DOG = 'FILTER_CREATED_DOG';
+export const CLEAR_DETAIL = 'CLEAR_DETAIL';
 
 
 
@@ -64,5 +69,39 @@ export function postDog() {
     } catch (error) {
         alert('Data missing')
     };
+};
+
+export function filterByName(payload) {
+    return {
+        type: FILTER_BY_NAME,
+        payload
+    }
+};
+
+export function filterByTemperaments(payload) {
+    return {
+        type: FILTER_BY_TEMPERAMENTS,
+        payload
+    }
+};
+
+export function filterByWeight(payload) {
+    return {
+        type: FILTER_BY_WEIGHT,
+        payload
+    }
+};
+
+export function filterCreatedDog(payload) {
+    return {
+        type: FILTER_CREATED_DOG,
+        payload
+    }
+};
+
+export function clearDetail() {
+    return {
+        type: CLEAR_DETAIL
+    }
 };
 
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -3,7 +3,12 @@ import {
     GET_DOG_NAME,
     GET_DOG_DETAIL,
     GET_ALL_TEMPERAMENTS,
-    POST_DOG
+    POST_DOG,
+    FILTER_BY_NAME,
+    FILTER_BY_TEMPERAMENTS,
+    FILTER_BY_WEIGHT,
+    FILTER_CREATED_DOG,
+    CLEAR_DETAIL
 } from '../actions/index'
 
 
@@ -43,6 +48,61 @@ function rootReducer(state = initialState, action) {
             };
 
         case POST_DOG:
+            return {
+                ...state
+            };
+
+        case FILTER_BY_NAME:
+            const filterDogs = action.payload === 'A-Z' ? state.dogs.sort((a, b) => {
+                if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+                if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+                return 0;
+            })
+            : state.dogs.sort((a, b) => {
+                if(a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+                if(a.name.toLowerCase() < b.name.toLowerCase()) return 1;
+                return 0;
+            });
+            return {
+                ...state,
+                dogs: filterDogs
+            };
+
+        case FILTER_BY_TEMPERAMENTS:
+            const allDogs = state.allDogs
+            const filterTemp = action.payload === 'All' ? allDogs : allDogs.filter(e => {
+                return e.temperament?.includes(action.payload)
+            });
+
+            return {
+                ...state,
+                dogs: filterTemp
+            };
+
+        case FILTER_BY_WEIGHT:
+            const allDogsWeight = state.allDogs.filter(e => e.weight_min)
+            const filterWeight = action.payload === 'min_weight' ? allDogsWeight.sort((a, b) => {
+                return a.weight_min - b.weight_min
+            })
+            : allDogsWeight.sort((a, b) => {
+                return a.weight_min - b.weight_min
+            }).reverse()
+
+            return {
+                ...state,
+                dogs: filterWeight
+            };
+
+        case FILTER_CREATED_DOG:
+            const allDogs2 = state.allDogs;
+            const filterCreated = action.payload === 'created' ? allDogs2.filter(e => e.createdInDb) : allDogs2.filter(e => !e.createdInDb);
+
+            return {
+                ...state,
+                dogs: actions.payload === 'all' ? state.allDogs2 : filterCreated
+            };
+
+        case CLEAR_DETAIL:
             return {
                 ...state
             };
